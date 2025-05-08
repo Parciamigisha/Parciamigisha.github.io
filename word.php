70a67ed180aecf96463459adfc140794d62d17a2
Parciamigisha <157398538+Parciamigisha@users.noreply.github.com>
2025-05-08 17:51:50 -0400

    Create word.php

---
+++ word.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Palindrome</title>
+</head>
+<body>
+	<center>
+		<h1>Palindrome Checker</h1>
+		<h2>This checks whether a word you have entered is a palindrome</h2>
+		<form name="palindrome" id="palindrome" method="post" action="word.php">
+			<label for="word" id="word">Enter A Word Or Phrase:</label>
+			<input type="text" name="word" id="word">
+			<p>
+				<b><input type="submit" name="submit" value="Submit"></b>
+			</p>
+		</form>
+	</center>
+</body>
+</html>
